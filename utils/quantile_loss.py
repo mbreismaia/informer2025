41a938212702dc60830cdee582585c74433dd40a
Beatriz Maia <prbeatrizmaia@gmail.com>
2025-07-05 03:00:57 -0300

    ajuste

--- utils/quantile_loss.py
+++ utils/quantile_loss.py
@@ -7,10 +7,12 @@ class QuantileLoss(nn.Module):
         self.quantiles = quantiles
 
     def forward(self, preds, target):
+        target = target.squeeze(-1) 
+
         losses = []
         for i, q in enumerate(self.quantiles):
-            pred_q = preds[:, :, i]
-            error = target - pred_q
+            pred_q = preds[:, :, i]  
+            error = target - pred_q 
             loss = torch.max((q - 1) * error, q * error)
-            losses.append(loss.unsqueeze(0))
-        return torch.cat(losses, dim=0).mean()
+            losses.append(loss.unsqueeze(0))  
+        return torch.cat(losses, dim=0).mean() 
