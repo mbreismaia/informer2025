2388cfb238693d6efe2ea2eb10feb4703aa30ced
Beatriz Maia <prbeatrizmaia@gmail.com>
2025-07-05 15:15:40 -0300

    Huber Loss Add

--- exp/exp_informer.py
+++ exp/exp_informer.py
@@ -7,7 +7,6 @@ from utils.metrics import metric
 from utils.quantile_loss import QuantileLoss
 
 import numpy as np
-
 import torch
 import torch.nn as nn
 from torch import optim
@@ -15,7 +14,6 @@ from torch.utils.data import DataLoader
 
 import os
 import time
-
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -25,62 +23,63 @@ class Exp_Informer(Exp_Basic):
     
     def _build_model(self):
         model_dict = {
-            'informer':Informer,
-            'informerstack':InformerStack,
+            'informer': Informer,
+            'informerstack': InformerStack,
         }
-        if self.args.model=='informer' or self.args.model=='informerstack':
-            e_layers = self.args.e_layers if self.args.model=='informer' else self.args.s_layers
-            model = model_dict[self.args.model](
-                self.args.enc_in,
-                self.args.dec_in, 
-                self.args.c_out, 
-                self.args.seq_len, 
-                self.args.label_len,
-                self.args.pred_len, 
-                self.args.factor,
-                self.args.d_model, 
-                self.args.n_heads, 
-                e_layers, # self.args.e_layers,
-                self.args.d_layers, 
-                self.args.d_ff,
-                self.args.dropout, 
-                self.args.attn,
-                self.args.embed,
-                self.args.freq,
-                self.args.activation,
-                self.args.output_attention,
-                self.args.distil,
-                self.args.mix,
-                self.device
-            ).float()
-        
+
+        e_layers = self.args.e_layers if self.args.model == 'informer' else self.args.s_layers
+
+        model = model_dict[self.args.model](
+            self.args.enc_in,
+            self.args.dec_in,
+            self.args.c_out,
+            self.args.seq_len,
+            self.args.label_len,
+            self.args.pred_len,
+            self.args.factor,
+            self.args.d_model,
+            self.args.n_heads,
+            e_layers,
+            self.args.d_layers,
+            self.args.d_ff,
+            self.args.dropout,
+            self.args.attn,
+            self.args.embed,
+            self.args.freq,
+            self.args.activation,
+            self.args.output_attention,
+            self.args.distil,
+            self.args.mix,
+            self.device
+        ).float()
+
         if self.args.use_multi_gpu and self.args.use_gpu:
             model = nn.DataParallel(model, device_ids=self.args.device_ids)
         return model
 
     def _get_data(self, flag):
         args = self.args
-
         data_dict = {
-            'ETTh1':Dataset_ETT_hour,
-            'ETTh2':Dataset_ETT_hour,
-            'ETTm1':Dataset_ETT_minute,
-            'ETTm2':Dataset_ETT_minute,
-            'WTH':Dataset_Custom,
-            'ECL':Dataset_Custom,
-            'Solar':Dataset_Custom,
-            'custom':Dataset_Custom,
+            'ETTh1': Dataset_ETT_hour,
+            'ETTh2': Dataset_ETT_hour,
+            'ETTm1': Dataset_ETT_minute,
+            'ETTm2': Dataset_ETT_minute,
+            'WTH': Dataset_Custom,
+            'ECL': Dataset_Custom,
+            'Solar': Dataset_Custom,
+            'custom': Dataset_Custom,
         }
         Data = data_dict[self.args.data]
-        timeenc = 0 if args.embed!='timeF' else 1
+        timeenc = 0 if args.embed != 'timeF' else 1
 
         if flag == 'test':
-            shuffle_flag = False; drop_last = True; batch_size = args.batch_size; freq=args.freq
-        elif flag=='pred':
-            shuffle_flag = False; drop_last = False; batch_size = 1; freq=args.detail_freq
+            shuffle_flag = False; drop_last = True; batch_size = args.batch_size; freq = args.freq
+        elif flag == 'pred':
+            shuffle_flag = False; drop_last = False; batch_size = 1; freq = args.detail_freq
             Data = Dataset_Pred
         else:
-            shuffle_flag = True; drop_last = True; batch_size = args.batch_size; freq=args.freq
+            shuffle_flag = True; drop_last = True; batch_size = args.batch_size; freq = args.freq
+
         data_set = Data(
             root_path=args.root_path,
             data_path=args.data_path,
@@ -93,6 +92,7 @@ class Exp_Informer(Exp_Basic):
             freq=freq,
             cols=args.cols
         )
+
         print(flag, len(data_set))
         data_loader = DataLoader(
             data_set,
@@ -104,13 +104,8 @@ class Exp_Informer(Exp_Basic):
         return data_set, data_loader
 
     def _select_optimizer(self):
-        model_optim = optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
-        return model_optim
-    
-    # def _select_criterion(self):
-        # criterion =  nn.MSELoss()
-        # return criterion
-    
+        return optim.Adam(self.model.parameters(), lr=self.args.learning_rate)
+
     def _select_criterion(self):
         if self.args.loss == 'quantile':
             print("📌 Usando QuantileLoss")
@@ -122,12 +117,10 @@ class Exp_Informer(Exp_Basic):
             print("📌 Usando MSELoss")
             return nn.MSELoss()
 
-
-
     def vali(self, vali_data, vali_loader, criterion):
         self.model.eval()
         total_loss = []
-        for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(vali_loader):
+        for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(vali_loader):
             pred, true = self._process_one_batch(
                 vali_data, batch_x, batch_y, batch_x_mark, batch_y_mark)
             loss = criterion(pred.detach().cpu(), true.detach().cpu())
@@ -137,21 +130,18 @@ class Exp_Informer(Exp_Basic):
         return total_loss
 
     def train(self, setting):
-        train_data, train_loader = self._get_data(flag = 'train')
-        vali_data, vali_loader = self._get_data(flag = 'val')
-        test_data, test_loader = self._get_data(flag = 'test')
+        train_data, train_loader = self._get_data(flag='train')
+        vali_data, vali_loader = self._get_data(flag='val')
+        test_data, test_loader = self._get_data(flag='test')
 
         path = os.path.join(self.args.checkpoints, setting)
-        if not os.path.exists(path):
-            os.makedirs(path)
+        os.makedirs(path, exist_ok=True)
 
         time_now = time.time()
-        
         train_steps = len(train_loader)
         early_stopping = EarlyStopping(patience=self.args.patience, verbose=True, delta=0.0001)
-        
         model_optim = self._select_optimizer()
-        criterion =  self._select_criterion()
+        criterion = self._select_criterion()
 
         if self.args.use_amp:
             scaler = torch.cuda.amp.GradScaler()
@@ -159,26 +149,27 @@ class Exp_Informer(Exp_Basic):
         for epoch in range(self.args.train_epochs):
             iter_count = 0
             train_loss = []
-            
+
             self.model.train()
             epoch_time = time.time()
-            for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(train_loader):
+
+            for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(train_loader):
                 iter_count += 1
-                
                 model_optim.zero_grad()
+
                 pred, true = self._process_one_batch(
                     train_data, batch_x, batch_y, batch_x_mark, batch_y_mark)
                 loss = criterion(pred, true)
                 train_loss.append(loss.item())
-                
-                if (i+1) % 100==0:
-                    print("\titers: {0}, epoch: {1} | loss: {2:.7f}".format(i + 1, epoch + 1, loss.item()))
-                    speed = (time.time()-time_now)/iter_count
-                    left_time = speed*((self.args.train_epochs - epoch)*train_steps - i)
-                    print('\tspeed: {:.4f}s/iter; left time: {:.4f}s'.format(speed, left_time))
+
+                if (i + 1) % 100 == 0:
+                    print(f"\titers: {i+1}, epoch: {epoch+1} | loss: {loss.item():.7f}")
+                    speed = (time.time() - time_now) / iter_count
+                    left_time = speed * ((self.args.train_epochs - epoch) * train_steps - i)
+                    print(f"\tspeed: {speed:.4f}s/iter; left time: {left_time:.4f}s")
                     iter_count = 0
                     time_now = time.time()
-                
+
                 if self.args.use_amp:
                     scaler.scale(loss).backward()
                     scaler.step(model_optim)
@@ -187,118 +178,96 @@ class Exp_Informer(Exp_Basic):
                     loss.backward()
                     model_optim.step()
 
-            print("Epoch: {} cost time: {}".format(epoch+1, time.time()-epoch_time))
+            print(f"Epoch: {epoch+1} cost time: {time.time()-epoch_time:.2f}s")
             train_loss = np.average(train_loss)
             vali_loss = self.vali(vali_data, vali_loader, criterion)
             test_loss = self.vali(test_data, test_loader, criterion)
 
-            print("Epoch: {0}, Steps: {1} | Train Loss: {2:.7f} Vali Loss: {3:.7f} Test Loss: {4:.7f}".format(
-                epoch + 1, train_steps, train_loss, vali_loss, test_loss))
+            print(f"Epoch: {epoch+1}, Steps: {train_steps} | "
+                  f"Train Loss: {train_loss:.7f} Vali Loss: {vali_loss:.7f} Test Loss: {test_loss:.7f}")
+
             early_stopping(vali_loss, self.model, path)
             if early_stopping.early_stop:
                 print("Early stopping")
                 break
 
-            adjust_learning_rate(model_optim, epoch+1, self.args)
-            
-        best_model_path = path+'/'+'checkpoint.pth'
+            adjust_learning_rate(model_optim, epoch + 1, self.args)
+
+        best_model_path = os.path.join(path, 'checkpoint.pth')
         self.model.load_state_dict(torch.load(best_model_path))
-        
         return self.model
 
     def test(self, setting):
         test_data, test_loader = self._get_data(flag='test')
-        
         self.model.eval()
-        
-        preds = []
-        trues = []
-        
-        for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(test_loader):
+
+        preds, trues = [], []
+
+        for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(test_loader):
             pred, true = self._process_one_batch(
                 test_data, batch_x, batch_y, batch_x_mark, batch_y_mark)
             preds.append(pred.detach().cpu().numpy())
             trues.append(true.detach().cpu().numpy())
 
-        preds = np.array(preds)
-        trues = np.array(trues)
-        print('test shape:', preds.shape, trues.shape)
-        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
-        trues = trues.reshape(-1, trues.shape[-2], trues.shape[-1])
-        print('test shape:', preds.shape, trues.shape)
+        preds = np.array(preds).reshape(-1, preds[0].shape[1], preds[0].shape[2])
+        trues = np.array(trues).reshape(-1, trues[0].shape[1], trues[0].shape[2])
 
-        # result save
-        folder_path = './results/' + setting +'/'
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
+        folder_path = f'./results/{setting}/'
+        os.makedirs(folder_path, exist_ok=True)
 
         mae, mse, rmse, mape, mspe = metric(preds, trues)
-        print('mse:{}, mae:{}'.format(mse, mae))
-
-        np.save(folder_path+'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
-        np.save(folder_path+'pred.npy', preds)
-        np.save(folder_path+'true.npy', trues)
+        print(f'mse: {mse}, mae: {mae}')
+        np.save(folder_path + 'metrics.npy', np.array([mae, mse, rmse, mape, mspe]))
+        np.save(folder_path + 'pred.npy', preds)
+        np.save(folder_path + 'true.npy', trues)
 
         return mae, mse
 
-
     def predict(self, setting, load=False):
         pred_data, pred_loader = self._get_data(flag='pred')
-        
+
         if load:
             path = os.path.join(self.args.checkpoints, setting)
-            best_model_path = path+'/'+'checkpoint.pth'
+            best_model_path = os.path.join(path, 'checkpoint.pth')
             self.model.load_state_dict(torch.load(best_model_path))
 
         self.model.eval()
-        
         preds = []
-        
-        for i, (batch_x,batch_y,batch_x_mark,batch_y_mark) in enumerate(pred_loader):
+
+        for i, (batch_x, batch_y, batch_x_mark, batch_y_mark) in enumerate(pred_loader):
             pred, true = self._process_one_batch(
                 pred_data, batch_x, batch_y, batch_x_mark, batch_y_mark)
             preds.append(pred.detach().cpu().numpy())
 
-        preds = np.array(preds)
-        preds = preds.reshape(-1, preds.shape[-2], preds.shape[-1])
-        
-        # result save
-        folder_path = './results/' + setting +'/'
-        if not os.path.exists(folder_path):
-            os.makedirs(folder_path)
-        
-        np.save(folder_path+'real_prediction.npy', preds)
-        
+        preds = np.array(preds).reshape(-1, preds[0].shape[1], preds[0].shape[2])
+        folder_path = f'./results/{setting}/'
+        os.makedirs(folder_path, exist_ok=True)
+        np.save(folder_path + 'real_prediction.npy', preds)
         return
 
     def _process_one_batch(self, dataset_object, batch_x, batch_y, batch_x_mark, batch_y_mark):
         batch_x = batch_x.float().to(self.device)
         batch_y = batch_y.float()
-
         batch_x_mark = batch_x_mark.float().to(self.device)
         batch_y_mark = batch_y_mark.float().to(self.device)
 
         # decoder input
-        if self.args.padding==0:
-            dec_inp = torch.zeros([batch_y.shape[0], self.args.pred_len, batch_y.shape[-1]]).float()
-        elif self.args.padding==1:
-            dec_inp = torch.ones([batch_y.shape[0], self.args.pred_len, batch_y.shape[-1]]).float()
-        dec_inp = torch.cat([batch_y[:,:self.args.label_len,:], dec_inp], dim=1).float().to(self.device)
-        # encoder - decoder
+        dec_inp = torch.zeros([batch_y.shape[0], self.args.pred_len, batch_y.shape[-1]]).float()
+        dec_inp = torch.cat([batch_y[:, :self.args.label_len, :], dec_inp], dim=1).float().to(self.device)
+
+        # forward
         if self.args.use_amp:
             with torch.cuda.amp.autocast():
-                if self.args.output_attention:
-                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
-                else:
-                    outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0] \
+                    if self.args.output_attention else self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
         else:
-            if self.args.output_attention:
-                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0]
-            else:
-                outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+            outputs = self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)[0] \
+                if self.args.output_attention else self.model(batch_x, batch_x_mark, dec_inp, batch_y_mark)
+
         if self.args.inverse:
             outputs = dataset_object.inverse_transform(outputs)
-        f_dim = -1 if self.args.features=='MS' else 0
-        batch_y = batch_y[:,-self.args.pred_len:,f_dim:].to(self.device)
+
+        f_dim = -1 if self.args.features == 'MS' else 0
+        batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
 
         return outputs, batch_y
--- models/model.py
+++ models/model.py
@@ -8,18 +8,17 @@ from models.decoder import Decoder, DecoderLayer
 from models.attn import FullAttention, ProbAttention, AttentionLayer
 from models.embed import DataEmbedding
 
+
 class Informer(nn.Module):
     def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
-                factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512, 
-                dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu', 
-                output_attention=False, distil=True, mix=True,
-                device=torch.device('cuda:0')):
+                 factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512, 
+                 dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu', 
+                 output_attention=False, distil=True, mix=True,
+                 device=torch.device('cuda:0')):
         super(Informer, self).__init__()
         self.pred_len = out_len
         self.attn = attn
         self.output_attention = output_attention
-
-        self.quantile_size = 3  
 
         self.enc_embedding = DataEmbedding(enc_in, d_model, embed, freq, dropout)
         self.dec_embedding = DataEmbedding(dec_in, d_model, embed, freq, dropout)
@@ -35,10 +34,10 @@ class Informer(nn.Module):
                     d_ff,
                     dropout=dropout,
                     activation=activation
-                ) for l in range(e_layers)
+                ) for _ in range(e_layers)
             ],
             [
-                ConvLayer(d_model) for l in range(e_layers - 1)
+                ConvLayer(d_model) for _ in range(e_layers - 1)
             ] if distil else None,
             norm_layer=torch.nn.LayerNorm(d_model)
         )
@@ -55,12 +54,13 @@ class Informer(nn.Module):
                     dropout=dropout,
                     activation=activation
                 )
-                for l in range(d_layers)
+                for _ in range(d_layers)
             ],
             norm_layer=torch.nn.LayerNorm(d_model)
         )
 
-        self.projection = nn.Linear(d_model, c_out * self.quantile_size, bias=True)
+        # Apenas 1 saída por timestamp (sem múltiplos quantis)
+        self.projection = nn.Linear(d_model, c_out, bias=True)
 
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, 
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
@@ -69,27 +69,24 @@ class Informer(nn.Module):
 
         dec_out = self.dec_embedding(x_dec, x_mark_dec)
         dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
-        dec_out = self.projection(dec_out)  # [B, seq_len, c_out * Q]
-        dec_out = dec_out.view(dec_out.shape[0], dec_out.shape[1], self.quantile_size)  # [B, seq_len, Q]
+        dec_out = self.projection(dec_out)  # [B, seq_len, c_out]
 
         if self.output_attention:
             return dec_out[:, -self.pred_len:, :], attns
         else:
-            return dec_out[:, -self.pred_len:, :]  # [B, L, Q]
+            return dec_out[:, -self.pred_len:, :]  # [B, pred_len, c_out]
 
 
 class InformerStack(nn.Module):
     def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len, 
-                factor=5, d_model=512, n_heads=8, e_layers=[3, 2, 1], d_layers=2, d_ff=512, 
-                dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
-                output_attention=False, distil=True, mix=True,
-                device=torch.device('cuda:0')):
+                 factor=5, d_model=512, n_heads=8, e_layers=[3, 2, 1], d_layers=2, d_ff=512, 
+                 dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
+                 output_attention=False, distil=True, mix=True,
+                 device=torch.device('cuda:0')):
         super(InformerStack, self).__init__()
         self.pred_len = out_len
         self.attn = attn
         self.output_attention = output_attention
-
-        self.quantile_size = 3  
 
         self.enc_embedding = DataEmbedding(enc_in, d_model, embed, freq, dropout)
         self.dec_embedding = DataEmbedding(dec_in, d_model, embed, freq, dropout)
@@ -107,10 +104,10 @@ class InformerStack(nn.Module):
                         d_ff,
                         dropout=dropout,
                         activation=activation
-                    ) for l in range(el)
+                    ) for _ in range(el)
                 ],
                 [
-                    ConvLayer(d_model) for l in range(el - 1)
+                    ConvLayer(d_model) for _ in range(el - 1)
                 ] if distil else None,
                 norm_layer=torch.nn.LayerNorm(d_model)
             ) for el in e_layers
@@ -130,12 +127,13 @@ class InformerStack(nn.Module):
                     dropout=dropout,
                     activation=activation
                 )
-                for l in range(d_layers)
+                for _ in range(d_layers)
             ],
             norm_layer=torch.nn.LayerNorm(d_model)
         )
 
-        self.projection = nn.Linear(d_model, c_out * self.quantile_size, bias=True)
+        # Apenas 1 saída por timestamp
+        self.projection = nn.Linear(d_model, c_out, bias=True)
 
     def forward(self, x_enc, x_mark_enc, x_dec, x_mark_dec, 
                 enc_self_mask=None, dec_self_mask=None, dec_enc_mask=None):
@@ -144,10 +142,9 @@ class InformerStack(nn.Module):
 
         dec_out = self.dec_embedding(x_dec, x_mark_dec)
         dec_out = self.decoder(dec_out, enc_out, x_mask=dec_self_mask, cross_mask=dec_enc_mask)
-        dec_out = self.projection(dec_out)  # [B, seq_len, c_out * Q]
-        dec_out = dec_out.view(dec_out.shape[0], dec_out.shape[1], self.quantile_size)  # [B, seq_len, Q]
+        dec_out = self.projection(dec_out)  # [B, seq_len, c_out]
 
         if self.output_attention:
             return dec_out[:, -self.pred_len:, :], attns
         else:
-            return dec_out[:, -self.pred_len:, :]  # [B, L, Q]
+            return dec_out[:, -self.pred_len:, :]  # [B, pred_len, c_out]
